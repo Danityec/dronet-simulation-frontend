2c7d2be8fc879048c4b1b873fbb49d57efac0e5d
Danityec <Danityec@gmail.com>
2022-03-21 18:51:09 +0200

    updated drone values

--- script.js
+++ script.js
@@ -1,38 +1,63 @@
-let x1 = 0
-let y1 = 0
+let x1 = 200
+let y1 = 200
 let z1 = 10
-let x2 = 100
-let y2 = 0
+let x2 = 500
+let y2 = 200
 let z2 = 10
-let x3 = 0
-let y3 = 100
+let x3 = 200
+let y3 = 500
 let z3 = 10
-let x4 = 100
-let y4 = 100
+let x4 = 500
+let y4 = 500
 let z4 = 10
+
+const MAX_REMOTE_VALUE = 720
+const MIN_REMOTE_VALUE = 0
+const MIN_SCALE_VALUE = 1.99
+const MAX_SCALE_VALUE = 0.01
+const MAX_SCREEN_Y_VALUE = 1200
+const MIN_SCREEN_Y_VALUE = 0
+const MAX_SCREEN_X_VALUE = 730
+const MIN_SCREEN_X_VALUE = 0
+
+let prevData = {
+    "yaw": 0,
+    "pitch": 0,
+    "roll": 0,
+    "throttle": 0
+}
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const scaleImg = (z) => {
-    const roundZ = Math.round(z)
-    z1 += roundZ
-    z2 += roundZ
-    z3 += roundZ
-    z4 += roundZ
+const yawAxis = (yaw) => {
+
 }
 
-const translateImg = (x, y) => {
-    const roundX = Math.round(x)
-    const roundY = Math.round(y)
-    x1 += roundX
-    y1 += roundY
-    x2 += roundX
-    y2 += roundY
-    x3 += roundX
-    y3 += roundY
-    x4 += roundX
-    y4 += roundY
+const rollPitchAxis = (roll, pitch) => {
+    const pitchVal = (roll - MIN_REMOTE_VALUE) * (MAX_SCREEN_Y_VALUE - MIN_SCREEN_Y_VALUE) / (MAX_REMOTE_VALUE - MIN_REMOTE_VALUE) + MIN_SCREEN_Y_VALUE;
+    const rollVal = (pitch - MIN_REMOTE_VALUE) * (MAX_SCREEN_X_VALUE - MIN_SCREEN_X_VALUE) / (MAX_REMOTE_VALUE - MIN_REMOTE_VALUE) + MIN_SCREEN_X_VALUE;
+
+    x1 = pitchVal - (z1*8)
+    y1 = rollVal - (z1*8)
+    x2 = pitchVal - (z1*8)
+    y2 = rollVal
+    x3 = pitchVal
+    y3 = rollVal - (z1*8)
+    x4 = pitchVal
+    y4 = rollVal
+    createDrawing();
+
+}
+
+const throttleAxis = (throttle) => {
+    const throttleVal = (throttle - MIN_REMOTE_VALUE) * (MAX_SCALE_VALUE - MIN_SCALE_VALUE) / (MAX_REMOTE_VALUE - MIN_REMOTE_VALUE) + MIN_SCALE_VALUE;
+
+    z1 *= throttleVal
+    z2 *= throttleVal
+    z3 *= throttleVal
+    z4 *= throttleVal
+    createDrawing();
 }
 
 const createDrawing = () => {
@@ -44,11 +69,24 @@ const createDrawing = () => {
 }
 
 ctx.fillStyle = "#FF0000";
+ctx.clearRect(0, 0, canvas.width, canvas.height)
+createDrawing();
 
-fetch(`https://dronet-simulation.herokuapp.com/api/remote/get`)
-    .then(response => response.json())
-    .then(result => {
-        translateImg(result.x, result.y);
-        scaleImg(result.z);
+async function fetchAsync() {
+    let response = await fetch(`https://dronet-simulation.herokuapp.com/api/remote/send`);
+    let data = await response.json();
+    if(prevData.pitch == data.pitch && prevData.yaw == data.yaw && prevData.roll == data.roll && prevData.throttle == data.throttle) {
+        return
+    }
+    else {
+        console.log(data)
+        if(prevData.pitch != data.pitch || prevData.roll != data.roll)
+            rollPitchAxis(data.roll, data.pitch);
+        if(prevData.throttle != data.throttle)      
+            throttleAxis(data.throttle);
+        prevData = data;
         createDrawing();
-    })
+    }
+}
+
+setInterval(fetchAsync, 1000);
